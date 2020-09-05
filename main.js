0ced34b339594afe7155c75de399bce513480e71
Cameron Romo <cromo1225@gmail.com>
2020-09-05 15:04:14 -0600

    Add global veriables to select radio values second form area and defined randomDish() and displayRandomSide()

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-function randomDish() {
-  var randomSide = sides[getRandomIndex(sides)];
-  var randomMain = mains[getRandomIndex(mains)];
-  var randomDessert = desserts[getRandomIndex(desserts)];
-}
+var secondForm = document.querySelector('.recipe');
+var side = document.querySelector('#side');
+var main = document.querySelector('#main-dish');
+var desert = document.querySelector('#dessert');
+var letsCookButton = document.querySelector('.lets-cook');
+
+letsCookButton.addEventListener('click', displayRandomSide);
+
+var newDish;
+
+function displayRandomSide() {
+randomDish(sides);
+secondForm.insertAdjacentHTML('afterbegin', `
+<h3>You should make:</h3>
+<h1>${newDish}</h1>`
+  );
+};
+
+function randomDish(dish) {
+  var newDish;
+  for (var i = 0; i < dish.length; i++) {
+    dish[getRandomIndex(dish)];
+  }
+  // var randomSide = sides[getRandomIndex(sides)];
+  // var randomMain = mains[getRandomIndex(mains)];
+  // var randomDessert = desserts[getRandomIndex(desserts)];
+};
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };

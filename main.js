774e25d59951d360d7190b26d0e4e2daef63f107
Cameron Romo <cromo1225@gmail.com>
2020-09-05 13:49:24 -0600

    Add randomDish()

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+function randomDish() {
+  var randomSide = sides[getRandomIndex(sides)];
+  var randomMain = mains[getRandomIndex(mains)];
+  var randomDessert = desserts[getRandomIndex(desserts)];
+}
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 };

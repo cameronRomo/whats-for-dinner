a15e68a02ff93f7f6906dcbef9ac4a6eaff61d46
Cameron Romo <cromo1225@gmail.com>
2020-09-07 20:08:37 -0600

    Add some experimental elements to displayRandom()

--- src/main.js
+++ src/main.js
@@ -1,17 +1,33 @@
 var secondForm = document.querySelector('.recipe');
 var cookPot = document.querySelector('.form2-display');
 var sideValue = document.getElementById('side');
-var mainDish = document.getElementById('main-dish')
+var mainDishValue = document.getElementById('main-dish')
 var letsCookButton = document.querySelector('.lets-cook');
-
-letsCookButton.addEventListener('click', displayRandomSide);
+var politeGest = document.querySelector('.polite-gesture')
+var randoDish = document.querySelector('.random-dish')
+letsCookButton.addEventListener('click', function() {
+  displayRandomSide();
+  displayRandomMain();
+});
 
 var newDish;
 
 function displayRandomSide() {
   event.preventDefault();
   if (sideValue.checked === true) {
-    randomDish(sides);
+    randomDish(mains);
+    cookPot.innerHTML = '';
+    secondForm.insertAdjacentHTML('afterbegin', `
+    <h3 class="polite-gesture">You should make:</h3>
+    <h1 class="random-dish">${newDish}!</h1>`
+    );
+  }
+};
+
+function displayRandomMain() {
+  event.preventDefault();
+  if (mainDishValue.checked === true) {
+    randomDish(mains);
     cookPot.innerHTML = '';
     secondForm.insertAdjacentHTML('afterbegin', `
     <h3 class="polite-gesture">You should make:</h3>
